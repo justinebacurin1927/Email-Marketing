1d361535b94a9d9108c12df07f08b29ca625157b
Justine Bacurin <justinebacurin1927@gmail.com>
2026-06-27 00:59:54 +0800

    Fix 419 CSRF on guest login; switch session driver to cookie for Render reliability

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'webhooks/mailgun/inbound',
+            'login/guest',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
