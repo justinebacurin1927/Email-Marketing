b0f142c12989531dcc8bfdd7581cbca662326693
Justine Bacurin <justinebacurin1927@gmail.com>
2026-06-27 01:11:22 +0800

    Force HTTPS scheme in production via AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Source;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 {
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
+
     View::composer('layouts.inbox-sidebar', function ($view) {
         $view->with('sources', Source::all());
     });
